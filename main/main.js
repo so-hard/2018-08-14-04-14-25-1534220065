c61bd104500905419fd8c36137280475023d78bd
so-hard <qq1136339397@gmail.com>
2019-05-29 13:18:02 +0800

    refactor: 重构代码并增加注释

--- main/main.js
+++ main/main.js
@@ -60,82 +60,61 @@ const LCDArray = [
 
 // console.log(map);
 
+function getLevel(LCDSecArr) {
+    // 分层
+    let firstFloor = ''; //第一层
+    let secondFloor = ''; //第二层
+    let threeFloor = ''; //第三层
+    LCDSecArr.forEach((val, index) => {
+        val.forEach((val, index) => {
+            switch (index) {
+                case 0:
+                    firstFloor += val;
+                    break;
+                case 1:
+                    secondFloor += val;
+                    break;
+                case 2:
+                    threeFloor += val;
+                    break;
+            }
+        });
+    });
+    return { firstFloor, secondFloor, threeFloor };
+}
+
+
+// 将ledInput 转为二位数组 
+function transArr(LCDInput) {
+    return LCDInput.map((val, index) => {
+        let arr = [];
+        let data = [...val];
+        for (let i = 0, len = data.length; i < len; i += 3) {
+            //三三为一组转成字符串，删除‘，’添加空格
+            arr.push(data.slice(i, i + 3).toString().replace(',', '').replace(',', '') + ' ');
+        }
+        return arr;
+    });
+}
+
+function delLast(arr) {
+    //删除字符串最后一个空格
+    let len = arr.length;
+    return arr.substr(0, len - 1)
+}
+
 module.exports = function main() {
+
     let result = '';//终极结果
     let LCDInput = [];
-    let LCDSecArr =[] //二维数组
-
-
-   
-    function delLast (arr) {
-         //删除字符串最后一位
-        // 巨坑无比
-        let len = arr.length;
-        return arr.substr(0,len-1)
-    }
-
-    // console.log(arguments);
-   
-    {
-    //将argunments 转为字符串
-    //根据输入的数字来获取对应的led图形放在LCDInput
-        [...arguments[0]].forEach(
-            (val) => {
-                LCDInput.push(LCDArray[val])
-            }
-        );
-    }
-
-
-
-
-
-    
-    {
-    // 将ledInput 转为二位数组 
-    // 然后分层
-    // 中间的空格剧坑无比
-         LCDSecArr = LCDInput.map(
-            (val, index) => {
-                let arr = [];
-                let data = [...val]
-                for (let i = 0, len = data.length; i < len; i += 3) {
-                    // arr.push(data.slice(i,i+3).concat());
-                    arr.push(data.slice(i, i + 3).toString().replace(',', '').replace(',', '') + ' ')
-                }
-                return arr
-            }
-        )
-
-        let firstFloor = ''; //第一层
-        let secondFloor = ''; //第二层
-        let threeFloor = '';   //第三层
-        LCDSecArr.forEach(
-            (val, index) => {
-                val.forEach(
-                    (val, index) => {
-                        switch (index) {
-                            case 0:
-                                firstFloor += val
-                                break;
-                            case 1:
-                                secondFloor += val
-                                break;
-                            case 2:
-                                threeFloor += val
-                                break;
-                        }
-                    }
-                )
-            }
-        )
-        //将每一层的最后一个空格去除
-        result = delLast(firstFloor) + '\n' + delLast(secondFloor) + '\n' + delLast(threeFloor) + '\n';
-    }
-
-
-
-
-
+    let LCDSecArr = []; //二维数组 
+    [...arguments[0]].forEach(
+        (val) => {
+            LCDInput.push(LCDArray[val])
+        }
+    );
+    LCDSecArr = transArr(LCDInput)
+    let { firstFloor, secondFloor, threeFloor } = getLevel(LCDSecArr);
+    result = delLast(firstFloor) + '\n' + delLast(secondFloor) + '\n' + delLast(threeFloor) + '\n';
     return result;
 };
